edc4f8a9822abce993edb435303a982f28e34fe1
Ryne Stajcar <rynostajcar@gmail.com>
2017-12-29 17:33:41 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,6 +15,8 @@
 def speak_to_grandma(voice)
   if voice == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  elsif voice == "WHAT DID YOU EAT TODAY?"
+    return "NO, NOT SINCE 1938!"
   else
     return "HUH?! SPEAK UP, SONNY!"
   end
